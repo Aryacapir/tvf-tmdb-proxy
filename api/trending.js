dff6086e26658895132d644c59bd7dff10d0779d
Aryacapir <ariagroup16@gmail.com>
2025-06-13 19:38:20 +0330

    Update trending.js

--- api/trending.js
+++ api/trending.js
@@ -1,22 +1,19 @@
 // api/trending.js
-export default async function handler(req, res) {
-  const apiKey = '05458cf40aa30c36be72aafe9d35d95e'; // کلید TMDB
-  // نوع مدیا: movie یا tv
-  const type = req.query.type || 'movie';
-  // بازه زمانی: day یا week
-  const time = req.query.time || 'day';
 
-  const url = `https://api.themoviedb.org/3/trending/${type}/${time}?api_key=${apiKey}&language=en-US`;
+module.exports = async (req, res) => {
+  const apiKey = '05458cf40aa30c36be72aafe9d35d95e';
+  const type   = req.query.type || 'movie';   // 'movie' یا 'tv'
+  const time   = req.query.time || 'day';     // 'day' یا 'week'
+  const url    = `https://api.themoviedb.org/3/trending/${type}/${time}?api_key=${apiKey}&language=en-US`;
+
   try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      return res
-        .status(response.status)
-        .json({ error: `TMDB returned ${response.status}` });
+    const tmdbRes = await fetch(url);
+    if (!tmdbRes.ok) {
+      return res.status(tmdbRes.status).json({ error: `TMDB returned ${tmdbRes.status}` });
     }
-    const data = await response.json();
-    return res.status(200).json(data);
+    const data = await tmdbRes.json();
+    res.status(200).json(data);
   } catch (err) {
-    return res.status(500).json({ error: err.message });
+    res.status(500).json({ error: err.message });
   }
-}
+};
